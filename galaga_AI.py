0373bb77cd8c8f5c436fae74037127f5e90974f9
George <georgefidler1709@gmail.com>
2018-12-02 17:05:37 +1100

    feed forward version for testing the rest works

--- galaga_AI.py
+++ galaga_AI.py
@@ -10,6 +10,8 @@ INITIAL_EPSILON = 1 # starting value of epsilon, used in exploration
 FINAL_EPSILON = 0.01 # final value of epsilon
 EPSILON_DECAY_STEPS = 50 # decay period
 
+MAX_STEPS = 1000
+
 STATE_DIM_1 = env.observation_space.shape[0]
 STATE_DIM_2 = env.observation_space.shape[1]
 STATE_DIM_3 = env.observation_space.shape[2]
@@ -41,39 +43,45 @@ POOL_STRIDE = 5
 # --------------------------------------------
 with tf.variable_scope('primary'):
 
-    conv = tf.layers.conv2d(
-        inputs=state_in,
-        filters=FILTERS,
-        kernel_size=FILTER_SIZE,
-        activation=tf.nn.relu,
-        padding='SAME',
-        reuse=None
-    )
-    print("conv = " + str(conv.shape))
+    # conv = tf.layers.conv2d(
+    #     inputs=state_in,
+    #     filters=FILTERS,
+    #     kernel_size=FILTER_SIZE,
+    #     activation=tf.nn.relu,
+    #     padding='SAME',
+    #     reuse=None
+    # )
+    #print("conv = " + str(conv.shape))
 
-    #fc1 = tf.layers.dense(state_in, fc1_units, activation=act, kernel_initializer=init)
+    fc1 = tf.layers.dense(state_in, fc1_units, activation=act, kernel_initializer=init)
     # fc2 = tf.layers.dense(fc1, fc2_units, activation=act, kernel_initializer=init)
     # fc3 = tf.layers.dense(fc2, fc2_units, activation=tf.nn.relu, kernel_initializer=tf.truncated_normal_initializer(stddev=0.1))
-    pooled = tf.nn.max_pool(
-        value=conv,
-        ksize=[1, POOL_FILTER_SIZE, POOL_FILTER_SIZE, 1],
-        strides=[1, POOL_STRIDE, POOL_STRIDE, 1],
-        padding='SAME'
-        )
 
-    done_conv = tf.reshape(tensor=pooled, shape=[-1, (round(STATE_DIM_1/POOL_STRIDE) * round(STATE_DIM_2/POOL_STRIDE) * FILTERS)])
+    # pooled = tf.nn.max_pool(
+    #     value=conv,
+    #     ksize=[1, POOL_FILTER_SIZE, POOL_FILTER_SIZE, 1],
+    #     strides=[1, POOL_STRIDE, POOL_STRIDE, 1],
+    #     padding='SAME'
+    #     )
+
+    # done_conv = tf.reshape(tensor=pooled, shape=[-1, (round(STATE_DIM_1/POOL_STRIDE) * round(STATE_DIM_2/POOL_STRIDE) * FILTERS)])
+    done_fc1 = tf.reshape(tensor=fc1, shape= [-1, STATE_DIM_1 * STATE_DIM_2 * fc1_units])
+
     #print("fc1 = " + str(fc1.shape))
 
     # TODO: Network outputs
     # output a q-value for EACH possible action, rank-1 tensor of length ACTION_DIM
-    q_values = tf.layers.dense(done_conv, ACTION_DIM, kernel_initializer=init) # linear activation
+
+    #q_values = tf.layers.dense(done_conv, ACTION_DIM, kernel_initializer=init) # linear activation
+    q_values = tf.layers.dense(done_fc1, ACTION_DIM, kernel_initializer=init) # linear activation
+
     # extract the q-value of the action in action_in
     # by using action_in as a mask
     ones = tf.ones_like(action_in)
     action_bool = action_in >= ones
     action_bool = tf.cast(action_bool, dtype=tf.float32) 
-    print("q_values = " + str(q_values.get_shape()))
-    print("action_bool = " + str(action_bool.get_shape()))
+    # print("q_values = " + str(q_values.get_shape()))
+    # print("action_bool = " + str(action_bool.get_shape()))
     q_action = tf.reduce_sum(tf.multiply(action_bool, q_values), reduction_indices=1)
 
     # action_chosen = tf.argmax(q_action)
@@ -96,28 +104,27 @@ def explore(state, epsilon):
     and assuming the network has already been defined, decide which action to
     take using e-greedy exploration based on the current q-value estimates.
     """
-    Q_estimates = q_values.eval(feed_dict={
-        state_in: [state]
-    })
 
     if random.random() <= epsilon:
         action = random.randint(0, ACTION_DIM - 1)
     else:
+        Q_estimates = q_values.eval(feed_dict={
+        state_in: [state]
+        })
         action = np.argmax(Q_estimates)
 
     one_hot_action = np.zeros(ACTION_DIM)
     one_hot_action[action] = 1
     return one_hot_action
 
-BATCH_SIZE = 120
-MAX_MEM_SIZE = 30000 # WARNING prob want this to be smaller to be effective
-TUPLE_DIM = 4 # each sample is a tupdle of (state, action, reward, next_state)
+BATCH_SIZE = 20
+MAX_MEM_SIZE = 100 # WARNING prob want this to be smaller to be effective
+TUPLE_DIM = 4 # each sample is a tuple of (state, action, reward, next_state)
 UPDATE_FREQ = 4 # TODO tune this for higher to make more stable
 
 memory = []
 # state is a tuple of (state, action, reward, next_state)
 def add_step_to_memory(state):
-    #print(state)
     memory.append(state)
     if(len(memory) > MAX_MEM_SIZE):
         memory.pop(0)
@@ -128,7 +135,7 @@ def get_batch_from_memory(batch_size):
 
 attempt_num = 0
 
-while True:
+while epsilon > 0.0001:
     t = 0
     done = False
     overall_score = 0
@@ -140,22 +147,24 @@ while True:
 
     state = env.reset()
 
-    while not done:
-        print("attempt = " + str(attempt_num) + " t = " + str(t))
+    while t < MAX_STEPS and not done:
+        t += 1
+        #env.render()
+        #print("attempt = " + str(attempt_num) + " t = " + str(t))
+        
         action = explore(state, epsilon)
         action_input = np.array(list(format(np.argmax(action), '0' + str(ACTION_OUTPUT_DIM) +'b')), dtype=np.float32)
         next_state, _, done, info = env.step(action_input)
 
         if done:
             next_state = None
+            print("Done")
 
         reward = info['score'] - overall_score
         overall_score = info['score']
         #if info['lives'] != lives:
         #   reward = reward + (info['lives'] - lives) * LIVES_PENALTY
         #   lives = info['lives']    
-        t += 1
-
         add_step_to_memory((state, action, reward, next_state))
 
         # perform training update after collecting some experience
@@ -192,10 +201,7 @@ while True:
                     targets[i] = s_reward
                 else:
                     targets[i] = s_reward + GAMMA * np.max(next_state_q_values[i])
-            print("t = " + str(t))
-            print("targets = " + str(targets.shape))
-            print("batch_actions = " + str(batch_actions.shape))
-            print("batch_states = " + str(batch_states.shape))
+
             # Do one training step
             session.run([optimizer], feed_dict={
                 target_in: targets, # ERROR this shape might be weird if you use commented out version
@@ -206,6 +212,6 @@ while True:
         # Update
         state = next_state
 
-    print("attempt: {0}, score: {1}", attempt_num, overall_score)
+    print("attempt: %d, score: %d" % (attempt_num, overall_score))
 
 env.close()
