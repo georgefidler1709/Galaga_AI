ab65ef5b4d906f84255af315028f72a4d1f782d0
George <georgefidler1709@gmail.com>
2018-12-25 08:43:20 +1100

    hanged from relu to elu as results showed output from the network whih was unresponsive to the state

--- galaga_AI.py
+++ galaga_AI.py
@@ -55,7 +55,7 @@ with tf.variable_scope('primary'):
         inputs=state_in,
         filters=FILTERS1,
         kernel_size=FILTER_SIZE1,
-        activation=tf.nn.relu,
+        activation=tf.nn.elu,
         padding='VALID',
         strides=FILTER_STRIDE1
     )
@@ -65,7 +65,7 @@ with tf.variable_scope('primary'):
         inputs=conv1,
         filters=FILTERS2,
         kernel_size=FILTER_SIZE2,
-        activation=tf.nn.relu,
+        activation=tf.nn.elu,
         padding='VALID',
         strides=FILTER_STRIDE2
     )
